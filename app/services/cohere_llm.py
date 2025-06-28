dca4120ef0a45f9aa635ec87b518c1ab84968f0e
Arsh-yusuf <134603893+Arsh-yusuf@users.noreply.github.com>
2025-06-28 23:43:23 +0530

    Update cohere_llm.py

--- app/services/cohere_llm.py
+++ app/services/cohere_llm.py
@@ -1,10 +1,17 @@
 import os
-from dotenv import load_dotenv
 import cohere
 
-load_dotenv()
-cohere_api_key = os.getenv("COHERE_API_KEY")
-co = cohere.Client(cohere_api_key)
+# For local development
+
+# from dotenv import load_dotenv
+
+# load_dotenv()
+# cohere_api_key = os.getenv("COHERE_API_KEY")
+# co = cohere.Client(cohere_api_key)
+
+COHERE_API_KEY = st.secrets["COHERE_API_KEY"]
+co = cohere.Client(COHERE_API_KEY)
+
 
 def answer_query(question: str, context: str) -> str:
     response = co.chat(
